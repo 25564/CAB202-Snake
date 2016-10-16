2907e243fc14955659fa35eb1331a862405d079c
25564 <cianoleary98@gmail.com>
2016-10-16 17:46:21 +1000

    Player Input

--- main.c
+++ main.c
@@ -11,6 +11,15 @@
 #include "graphics.h"
 #include "lcd.h"
 
+enum Directions // Snake Direction
+{
+    IDLE,
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
 // The standard Sprite was too bloated. Not even sorry
 typedef struct sprite {
 	float x, y;				// Position of top-left sprite corner
@@ -75,8 +84,33 @@ void trimList(ListNode * head, int Start){ // Delete all after a certain point
 
 // Initial Vars
 
-unsigned char PlayerLives = 0;
+int PlayerLives = 5;
+int PlayerScore = 0;
+
+// Initial Snake Vars
 ListNode * SnakeLinkedList = NULL;
+unsigned char SnakeLength = 1;
+enum Directions SnakeDirection;
+
+
+// Helper Drawing Functions
+
+void DrawHUD() {
+	draw_string(30, 0, "L: ");
+	draw_char(40, 0, (char)PlayerLives + '0');
+    draw_string(0, 0, "S: ");
+	draw_char(10, 0, (char)PlayerScore + '0');
+	draw_string(60, 0, "D: ");
+	draw_char(70, 0, (char)SnakeDirection + '0');
+}
+
+
+// Snake Controls 
+void SnakeLoseLife() {
+	PlayerLives = PlayerLives - 1;
+	SnakeDirection = IDLE;
+}
+
 
 
 void initial_screen() {
@@ -99,6 +133,18 @@ void initial_setup() {
 
 	lcd_init(LCD_DEFAULT_CONTRAST);
 
+    // Configure timer
+    TCCR0B &= ~((1<<WGM02));
+
+    //Prescaler = 256
+    //Timer Speed = 128 microseconds
+    //Timer Overflow Speed = 8160 micro seconds
+    TCCR0B |= (1<<CS02);
+    TCCR0B &= ~((1<<CS01));
+    TIMSK0 |= (1 << TOIE0);
+
+	sei(); // Globally enable interrupts
+	
 	// Debugging Linked List
 	int test = 0;
 
@@ -112,10 +158,57 @@ void initial_setup() {
 	// End Debugging Linked List
 }
 
+// Interrupt that
+// Checks for user input
+ISR(TIMER0_OVF_vect) {
+	// SW2
+	if ((PINF >> 6) & 1){
+	}
+
+	// SW3
+	if ((PINF >> 5) & 1){
+	}
+
+	// Left
+	if ((PINB >> 1) & 1) {
+		if (SnakeDirection == RIGHT){
+			SnakeLoseLife();
+			return;
+		}
+		SnakeDirection = LEFT;
+	}
+
+	// Right
+	if (PIND & 1) {
+		if (SnakeDirection == LEFT){
+			SnakeLoseLife();
+			return;
+		}
+		SnakeDirection = RIGHT;
+	}
+
+	// Down
+	if ((PINB >> 7) & 1) {
+		if (SnakeDirection == UP){
+			SnakeLoseLife();
+			return;
+		}
+		SnakeDirection = DOWN;
+	}
+
+	// Up
+	if ((PIND >> 1) & 1){
+		if (SnakeDirection == DOWN){
+			SnakeLoseLife();
+			return;
+		}
+		SnakeDirection = UP;
+	}
+}
 
 void update(){
 	clear_screen();
-
+	DrawHUD();
 	show_screen();
 }
 
